9923ec4edb9fa93df7bafc13cbae09401f28417b
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-26 23:41:00 -0500

    Create webpack setup

---
+++ webpack.common.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Template',
+            inject: 'head',
+            scriptLoading: 'defer',
+        }),
+    ],
+};
