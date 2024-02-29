618ea3029c4b678ecdddbf6cae021fd295e9210a
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-29 10:07:12 -0500

    Add the babel-loader to the module property

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,26 @@
 const { merge } = require('webpack-merge');
-const common = require(./webpack.common.js);
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         static: './src',
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          }
+        ]
     },
 });
